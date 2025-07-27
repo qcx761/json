a89ed356e46c51d715dd9b026a14ab2bee749a5f
qcx761 <3216563979@qq.com>
2025-07-28 00:33:18 +0800

    1

---
+++ 3.cpp
@@ -0,0 +1,20 @@
+#include "Person.pb.h"
+#include <iostream>
+
+int main() {
+    Person p;
+    p.set_id(1);
+    p.set_name("李四");
+    p.set_sex("man");
+    p.set_age(18);
+
+    std::string output;
+    p.SerializeToString(&output);
+    std::cout << "序列化后: " << output << std::endl;
+
+    Person pp;
+    pp.ParseFromString(output);
+    std::cout << "名字: " << pp.name() << ", 年龄: " << pp.age()
+              << ", 性别: " << pp.sex() << ", id: " << pp.id() << std::endl;
+    return 0;
+}
